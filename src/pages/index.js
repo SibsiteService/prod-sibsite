797a62e6e29e673f016359f18c44ad10fe1773b2
Kuznetsov <denis.kuznetsov01@metronom.com>
2019-09-23 18:01:58 +0200

    new commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -85,16 +85,13 @@ const IndexPage = () => (
         <div className="inner">
           <h2 className="major">Наши цены</h2>
           <p>
-            Cras mattis ante fermentum, malesuada neque vitae, eleifend erat.
-            Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis
-            egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus
-            in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.
+            Чтобы подробнее ознакомиться с нащими ценами нажмите на кнопку ниже.
           </p>
           
           <ul className="actions">
             <li>
-              <a href="/#" className="button">
-                Browse All
+              <a href="/Prices" className="button">
+                Цены
               </a>
             </li>
           </ul>
