1fe50c6e0f7a86a9c16c46747797a9f43c8e1a30
Kuznetsov <denis.kuznetsov01@metronom.com>
2019-09-26 10:56:56 +0200

    add gtm

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,5 +27,26 @@ module.exports = {
         trackingId: "UA-148714105-2",
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-NSNGV84",
+  
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+  
+        // Specify optional GTM environment details.
+        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        dataLayerName: "YOUR_DATA_LAYER_NAME",
+      },
+    },
+    
   ],
 };
